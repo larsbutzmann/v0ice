5a119a4e04b4d8b536979b298eeedf2bbbd850bf
kjellski <otto.kjell@gmail.com>
2013-06-01 15:43:25 +0200

    merged to both features

--- client/v0ice.js
+++ client/v0ice.js
@@ -1,104 +1,21 @@
 Files = new Meteor.Collection("files");
 
-function __log(e, data) {
-  console.log("\n" + e + " " + (data || ''));
-}
-
-var audio_context;
-var recorder;
-var recording_state = false;
-
-function startUserMedia(stream) {
-  var input = audio_context.createMediaStreamSource(stream);
-  __log('Media stream created.');
-  
-
-  var zeroGain = audio_context.createGain();
-  zeroGain.gain.value = 0;
-  input.connect(zeroGain);
-  zeroGain.connect(audio_context.destination);
-  __log('Input connected to muted gain node connected to audio context destination.');
-
-  recorder = new Recorder(input);
-  __log('Recorder initialised.');
-
-  // input.connect(audio_context.destination);
-  // __log('Input connected to audio context destination.');
-  
-  // recorder = new Recorder(input);
-  // __log('Recorder initialised.');
-}
-
-function createDownloadLink() {
-  recorder && recorder.exportWAV(function(blob) {
-    var url = URL.createObjectURL(blob);
-    var li = document.createElement('li');
-    var au = document.createElement('audio');
-    var hf = document.createElement('a');
-    
-    au.controls = true;
-    au.src = url;
-    hf.href = url;
-    hf.download = new Date().toISOString() + '.wav';
-    // Recorder.forceDownload(blob, "test.wav");
-    hf.innerHTML = hf.download;
-    li.appendChild(au);
-    li.appendChild(hf);
-    // recordingslist.appendChild(li);
-  });
-}
-
-Template.mic.rendered = function () {
-  $('#record-btn').click(function () {
-    $(this).toggleClass("record-started");
-  });
-
-  try {
-    // webkit shim
-    window.AudioContext = window.AudioContext || window.webkitAudioContext;
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
-    window.URL = window.URL || window.webkitURL;
-    
-    audio_context = new AudioContext;
-    __log('Audio context set up.');
-    __log('navigator.getUserMedia ' + (navigator.getUserMedia ? 'available.' : 'not present!'));
-  } catch (e) {
-    alert('No web audio support in this browser!');
-  }
-  
-  navigator.getUserMedia({audio: true}, startUserMedia, function(e) {
-    __log('No live audio input: ' + e);
-  });
-};
-
-Template.mic.events({
-  'click #record-btn' : function () {
-    // template data, if any, is available in 'this'
-    if (recording_state !== true) {
-      recorder.record();
-      recording_state = true;
-      console.log("record started");
-    } else {
-      recorder.stop();
-      file = {
-        name: ".wav",
-        url: document.URL,
-        timestamp: (new Date()).getTime()
-      }
-      console.log(file);
-      Meteor.call("addFile", file, function(err, result) {
-          if (result) {
-              console.log("Successfully added new record with auto_inc id " + result);
-          }
-      });
-      // createDownloadLink();
-      recording_state = false;
-      console.log("record stopped");
-    } 
-  }
-});
-
-// show files 
 Template.files.files = function () {
   return Files.find({}, {limit: 5, sort: {name: 1}});
 };
+
+Meteor.Router.add({
+  '/': 'landingPage'
+});
+
+Template.body.helpers({
+  layoutName: function() {
+    switch (Meteor.Router.page()) {
+      case 'landingPage':
+        return 'landing';
+      default:
+        return 'landing';
+    }
+  }
+});
+
