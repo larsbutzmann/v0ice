ff6aba5881395314d9ccd899fc7efd02a160d7bb
Lars Butzmann <lars.butzmann@gmail.com>
2013-06-01 18:16:13 +0200

    files are now playable in list view

--- client/fileList.js
+++ client/fileList.js
@@ -1,5 +1,6 @@
 Files = new Meteor.Collection("files");
 
 Template.fileIndex.files = function () {
-  return Files.find({}, {limit: 5, sort: {name: 1}});
+  	f = Files.find({}, {limit: 10});
+    return f;
 };
