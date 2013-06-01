bbe566e69419f80de3e53c6413dacd728011093e
kjellski <otto.kjell@gmail.com>
2013-06-01 12:33:29 +0200

    added bootstrap underscore and jquery

--- client/v0ice.js
+++ client/v0ice.js
@@ -1,13 +1,15 @@
-Template.mic.greeting = function () {
-  return "TODO: this needs to be round tripped to match against customer/user";
+Template.mic.rendered = function () {
+  $('#record-btn').click(function () {
+    $(this).toggleClass("record-started");
+  });
 };
+
 
 Template.mic.events({
   'click #record-btn' : function () {
-    console.log("record started");
-    this.recording = true;
     // template data, if any, is available in 'this'
-    if (typeof console !== 'undefined')
-      console.log("You pressed the button");
+    console.log("record started");
+    
+    
   }
 });
